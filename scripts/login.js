d33b43bb7b87405f091bf40a7ffd4c97d645f8be
murilodotdev <murilo.for.work@gmail.com>
2025-09-30 17:39:44 -0300

    fix: update card title selection and improve message formatting in login script

--- scripts/login.js
+++ scripts/login.js
@@ -1,11 +1,11 @@
 const loginForm = document.getElementById("login")
 loginForm.addEventListener("submit", (e)=>{
     e.preventDefault()
-    let cardTitle = document.getElementById("card-title")
+    let cardTitle = document.getElementsByClassName("card-title")[0]
     cardTitle.innerText = "Site ainda em construção 🚧👷"
 
     let msg = document.createElement("p")
-    msg.innerHTML = "Foi mal, não vou poder te ajudar hoje... 😢 <br> Mas vem cortar o cabelin com nois pae 🤙😜"
+    msg.innerText = "Foi mal, não vou poder te ajudar hoje... 😢 Mas vem cortar o cabelin com nois pae 🤙😜"
     msg.style = "margin-top: 12px;"
 
     loginForm.replaceWith(msg)
